024ddd49901b906f7845cf9b2aeaffa6a5452b07
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-10 08:48:04 +0000

    class name added

--- award-app/src/components/Hero.jsx
+++ award-app/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='relative h-dvh w-screen overflow-x-hidden'>
         <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
-            <div className=''>
+            <div className='mask-clip-path'>
 
             </div>
         </div>

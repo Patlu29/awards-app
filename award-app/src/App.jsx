bc78172610ba6e98a3dbaf246b66c8bb85e7f0bf
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-10 23:47:35 +0000

    code arrangements changed

--- award-app/src/App.jsx
+++ award-app/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;

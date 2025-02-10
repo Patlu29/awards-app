e508aa194648cc35cfc26050577b66f948f5b128
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-10 08:47:35 +0000

    div added

--- award-app/src/components/Hero.jsx
+++ award-app/src/components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+        <div>
+            <div className=''>
+
+            </div>
+        </div>
+        </div>
+    </div>
   )
 }
 

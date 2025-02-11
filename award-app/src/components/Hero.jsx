6d609464608710692aa0cbcf9d26dec37e5f2a0e
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-11 00:17:27 +0000

    page top content added

--- award-app/src/components/Hero.jsx
+++ award-app/src/components/Hero.jsx
@@ -67,8 +67,19 @@ const Hero = () => {
           />
         </div>
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          G<b>a</b>ming
+          <b>gaming</b>
         </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              <b>redifine</b>
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter Into The Virtual World <br />
+              Unleash The Domain Of Gaming
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );

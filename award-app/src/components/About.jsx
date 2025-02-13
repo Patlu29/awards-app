13878bb433ade1524ead35103698fade155111cb
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-13 00:26:31 +0000

    about section coded

--- award-app/src/components/About.jsx
+++ award-app/src/components/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to kalahari
+        </h2>
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          <b>discover the virtual world of gaming</b>
+        </div>
+        <div className="about-subtext">
+          <p>The Game of Saving Your Life Is Begins Now</p>
+          <p>Kalahari Is The Field of a Game With a Countless Players</p>
+        </div>
+      </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;

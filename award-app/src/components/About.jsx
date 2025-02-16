d1ef34592a1379b2eb45add833be3f2270aad10f
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-16 07:34:12 +0000

    image added for about

--- award-app/src/components/About.jsx
+++ award-app/src/components/About.jsx
@@ -8,6 +8,17 @@ const About = () => {
       <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
         <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b> <b>virtual</b> <b>gaming</b>
       </div>
+      <div className='about-subtext'>
+    <p>The Game of Survival Begins...!</p>
+    <p>Skipiya Unites All Players in a Virtual World</p>
+      </div>
+      </div>
+      <div className='h-dvh w-screen' id='clip'>
+        <div className='mask-clip-path about-image'>
+        <img src="img/about.webp" alt="background"
+        className='absolute left-0 top-0 size-full object-cover' />
+        </div>
+        
       </div>
     </div>
   )

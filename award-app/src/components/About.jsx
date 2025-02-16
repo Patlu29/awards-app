ab46552ebdcb8a4d46d9b3ba2de16654c364b786
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-16 00:45:20 +0000

    AnimatedTitle deleted and about page will started from scratch

--- award-app/src/components/About.jsx
+++ award-app/src/components/About.jsx
@@ -1,55 +1,16 @@
-import { useGSAP } from "@gsap/react";
-import React from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import AnimatedTitle from "./AnimatedTitle";
-
-gsap.registerPlugin(ScrollTrigger);
+import React from 'react'
 
 const About = () => {
-  useGSAP(() => {
-    const clipAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#clip",
-        start: "center center",
-        end: "+=800 center",
-        scrub: 0.5,
-        pin: true,
-        pinSpacing: true,
-      },
-    });
-    clipAnimation.to(".mask-clip-path", {
-      width: "100vw",
-      height: "100vh",
-      borderRadius: 0,
-    });
-  });
   return (
-    <div id="about" className="min-h-screen w-screen">
-      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[30px]">
-          Welcome to kalahari
-        </h2>
-        <AnimatedTitle
-          title="<b>discover</b> <b>the<b/> <b>virtual<b/> <b>world<b/> <b>of<b/> <b>gaming<b/>"
-          containerClass="mt-5 !text-black text-center"
-        />
-        <div className="about-subtext">
-          <p>The Game of Saving Your Life Is Begins Now</p>
-          <p>Kalahari is The Field of a Survival Game With a Countless Players</p>
-        </div>
+    <div id='about' className='min-h-screen w-screen'>
+      <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+      <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Skipiya</h2>
+      <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+        <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b> <b>virtual</b> <b>gaming</b>
       </div>
-      <div className="h-dvh w-screen" id="clip">
-        <div className="mask-clip-path about-image">
-          <img
-            src="img/about.webp"
-            alt="background"
-            className="absolute left-0 top-0 size-full object-cover"
-          />
-        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About

8d896ad3f42bf9c86aadaa2c878ba4e66763e483
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-16 07:43:25 +0000

    allignment changed

--- award-app/src/components/About.jsx
+++ award-app/src/components/About.jsx
@@ -1,50 +1,55 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import React from 'react'
-import { ScrollTrigger } from 'gsap/all'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { ScrollTrigger } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  useGSAP( () => {
+  useGSAP(() => {
     const clipAnimation = gsap.timeline({
       scrollTrigger: {
-        trigger: '#clip',
-        start: 'center center',
-        end: '+=800 center',
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
         scrub: 0.5,
         pin: true,
         pinSpacing: true,
-      }
-    })
+      },
+    });
 
-    clipAnimation.to('.mask-clip-path', {
-      width: '100vw',
-      height: '100vh',
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
       borderRadius: 0,
-    })
-  })
+    });
+  });
   return (
-    <div id='about' className='min-h-screen w-screen'>
-      <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-      <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Skipiya</h2>
-      <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-        <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b> <b>virtual</b> <b>gaming</b>
-      </div>
-      <div className='about-subtext'>
-    <p>The Game of Survival Begins...!</p>
-    <p>Skipiya Unites All Players in a Virtual World</p>
-      </div>
-      </div>
-      <div className='h-dvh w-screen' id='clip'>
-        <div className='mask-clip-path about-image'>
-        <img src="img/about.webp" alt="background"
-        className='absolute left-0 top-0 size-full object-cover' />
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Skipiya
+        </h2>
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b>{" "}
+          <b>virtual</b> <b>gaming</b>
         </div>
-        
+        <div className="about-subtext">
+          <p>The Game of Survival Begins...!</p>
+          <p>Skipiya Unites All Players in a Virtual World</p>
+        </div>
+      </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;

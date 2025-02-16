caedfea85fb6dc51d9e12790c500afd4d6cc87af
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-16 09:50:22 +0000

    title changed to animated title file

--- award-app/src/components/About.jsx
+++ award-app/src/components/About.jsx
@@ -32,10 +32,6 @@ const About = () => {
           Welcome to Skipiya
         </h2>
         <AnimatedTitle />
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b>{" "}
-          <b>virtual</b> <b>gaming</b>
-        </div>
         <div className="about-subtext">
           <p>The Game of Survival Begins...!</p>
           <p>Skipiya Unites All Players in a Virtual World</p>
--- award-app/src/components/AnimatedTitle.jsx
+++ award-app/src/components/AnimatedTitle.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const AnimatedTitle = () => {
   return (
-    <div>AnimatedTitle</div>
-  )
-}
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      <b>discover</b> <b>the</b> <b>world's</b> <br /> <b>largest</b>{" "}
+      <b>virtual</b> <b>gaming</b>
+    </div>
+  );
+};
 
-export default AnimatedTitle
+export default AnimatedTitle;

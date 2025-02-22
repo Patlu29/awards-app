a90c3d5623afc496c3d8e959ef2ea20d25458b3a
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-22 23:58:33 +0000

    div place changed and new feature-2 added

--- award-app/src/components/Features.jsx
+++ award-app/src/components/Features.jsx
@@ -13,6 +13,9 @@ const BentoCard = ({ src, title, description, isCommingSoon }) => {
       <div className="relative z-10 flex-size-full flex-col  justify-between p-5 text-blue-50">
         <div>
           <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
         </div>
       </div>
     </div>
@@ -27,14 +30,14 @@ const Features = () => {
           <p className="font-circular-web text-lg text-blue-50">
             Into The Visual World Layer
           </p>
-        </div>
+
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
           🚀 Dive into a world of adventure, strategy, and stunning animations.
           Whether you're here to challenge yourself, explore new realms, or just
           have fun, it brings a seamless and visually captivating experience
           right to your browser.
         </p>
-      </div>
+        </div>
       <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
         <BentoCard
           src="videos/feature-1.mp4"
@@ -44,8 +47,18 @@ const Features = () => {
             </>
           }
           description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
-          isCommingSoon
         />
+      </div>
+      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+<div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+<BentoCard 
+src="videos/feature-2.mp4"
+title={<><b>sigma</b></>}
+description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
+
+/>
+</div>
+      </div>
       </div>
     </section>
   );

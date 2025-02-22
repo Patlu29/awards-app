62fe539784e87837e43b7263b3e06bd99ea256c7
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-22 23:59:13 +0000

    allignment corrected

--- award-app/src/components/Features.jsx
+++ award-app/src/components/Features.jsx
@@ -31,34 +31,37 @@ const Features = () => {
             Into The Visual World Layer
           </p>
 
-        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          🚀 Dive into a world of adventure, strategy, and stunning animations.
-          Whether you're here to challenge yourself, explore new realms, or just
-          have fun, it brings a seamless and visually captivating experience
-          right to your browser.
-        </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            🚀 Dive into a world of adventure, strategy, and stunning
+            animations. Whether you're here to challenge yourself, explore new
+            realms, or just have fun, it brings a seamless and visually
+            captivating experience right to your browser.
+          </p>
         </div>
-      <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
-        <BentoCard
-          src="videos/feature-1.mp4"
-          title={
-            <>
-              <b>radint</b>
-            </>
-          }
-          description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
-        />
-      </div>
-      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-<div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
-<BentoCard 
-src="videos/feature-2.mp4"
-title={<><b>sigma</b></>}
-description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
-
-/>
-</div>
-      </div>
+        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                <b>radint</b>
+              </>
+            }
+            description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
+          />
+        </div>
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  <b>sigma</b>
+                </>
+              }
+              description="A cross-platform meta game app, turning your activities across Web2 and Web3 games into a rewarding adventure"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );

7de1a04586f2ba3e870cb54b2fea5b08f4f46ec5
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-24 00:13:50 +0000

    tilt animation created for each feature

--- award-app/src/components/Features.jsx
+++ award-app/src/components/Features.jsx
@@ -1,9 +1,34 @@
-import React, { Children } from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 
 const BentoTilt = ({ children, className = " " }) => {
-  return <div className={className}>{children}</div>;
+
+  const [transformStyle, setTransformStyle] = useState('')
+
+  const itemRef = useRef()
+
+  const handleMouseMove = (e) => {
+    if (!itemRef.current) return
+
+    const {left, top, width, height} = itemRef.current.getBoundingClientRect()
+
+    const relativeX = (e.clientX - left) / width
+    const relativeY = (e.clientY - top) / height
+
+    const tiltX = (relativeY - 0.5) * 15
+    const tiltY = (relativeX - 0.5) * -15
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`
+
+    setTransformStyle(newTransform)
+  }
+
+  const handleMouseLeave = () => {
+    setTransformStyle('')
+  }
+
+  return <div className={className}
+  ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}}>{children}</div>;
 };
 
 const BentoCard = ({ src, title, description }) => {

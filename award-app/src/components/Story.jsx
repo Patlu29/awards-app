1d9cb1252c700734f2b58118f75554d44835805f
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-02-25 23:46:02 +0000

    arranged code with the correct indentation

--- award-app/src/components/Story.jsx
+++ award-app/src/components/Story.jsx
@@ -1,74 +1,79 @@
-import React, { useRef } from 'react'
-import AnimatedTitle from './AnimatedTitle'
-import gsap from 'gsap'
+import React, { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
+import gsap from "gsap";
 
 const Story = () => {
-  const frameRef = useRef('null')
+  const frameRef = useRef("null");
 
-   const handleMouseLeave = () => {
-
-    const element = frameRef.current
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
 
     gsap.to(element, {
       duration: 0.3,
       rotateX: 0,
       rotateY: 0,
-      ease: 'power1.inOut',
-    })
-   }
+      ease: "power1.inOut",
+    });
+  };
 
-   const handleMouseMove = (e) => {
-    const { clientX, clientY } = e
-    const element = frameRef.current
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
 
-    if(!element) return
+    if (!element) return;
 
-    const rect = element.getBoundingClientRect()
-    const x = clientX - rect.left
-    const y = clientY - rect.top
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
 
-    const centerX = rect.width / 2
-    const centerY = rect.height / 2
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
 
-    const rotateX = ((y - centerY) / centerY) * -10
-    const rotateY = ((x - centerX) / centerX) * 10
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
 
     gsap.to(element, {
       duration: 0.3,
-      rotateX, rotateY,
+      rotateX,
+      rotateY,
       transformPerspective: 500,
-      ease: 'power1.inOut',
-    })
-   }
+      ease: "power1.inOut",
+    });
+  };
 
   return (
-    <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
-      <div className='flex size-full flex-col items-center py-10 pb-24'>
-<p className='font-general text-sm uppercase md:text-[10px]'>Multiversal Virtual World</p>
-<div className='relative size-full'>
-  <AnimatedTitle title='<b>the</b> <b>story</b> <b>of</b> <br /> <b>a</b> <b>hidden</b> <b>realm</b>'
-  sectionId='#story'
-  containerClass='mt-5 pointer-events-none mix-blend-difference relative z-10' />
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Multiversal Virtual World
+        </p>
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="<b>the</b> <b>story</b> <b>of</b> <br /> <b>a</b> <b>hidden</b> <b>realm</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
 
-  <div className='story-img-container'>
-    <div className='story-img-mask'>
-      <div className='story-img-content'>
-        <img 
-        ref={frameRef}
-        src='/img/entrance.webp'
-        alt='Entrance'
-        className='object-contain'
-        onMouseLeave={handleMouseLeave}
-        onMouseUp={handleMouseLeave}
-        onMouseEnter={handleMouseLeave}
-        onMouseMove={handleMouseMove} />
-      </div>
-    </div>
-  </div>
-</div>
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="Entrance"
+                  className="object-contain"
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Story
+export default Story;
